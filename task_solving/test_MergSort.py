0c15d0084c898be70376f2ee204799e5f81a64fa
Oukey <oukey311@gmail.com>
2019-10-05 10:43:58 +0400

    Сортировка кучей. Разработано решение, преверено тестами

--- task_solving/test_MergSort.py
+++ task_solving/test_MergSort.py
@@ -1,7 +1,19 @@
 import unittest
-import MergeSort
+import MergeSort as MS
 
 
 class MergeSortTest(unittest.TestCase):
+
+    def NormTest(self):
+        heap = MS.Heap()
+        self.assertEqual(heap.HeapSize, 0)
+        self.assertEqual(heap.HeapArray, [])
+        heap.Add(1, 101)
+        self.assertEqual(heap.HeapSize, 1)
+        self.assertEqual(heap.HeapArray, [101])
+
+
+
+
     if __name__ == '__main__':
         unittest.main()
