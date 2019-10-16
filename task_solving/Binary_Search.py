6c8a042437d4d470f8fae0f1118c459a668bf115
Oukey <oukey311@gmail.com>
2019-10-16 19:07:12 +0400

    Двоичный поиск. Работает

--- task_solving/Binary_Search.py
+++ task_solving/Binary_Search.py
@@ -9,67 +9,20 @@ class BinarySearch:
 
     def Step(self, N):
         '''Метод выполнения одного шага поиска. Прнимает искомое число (int)'''
-        step = (self.Left + self.Right + 1) // 2
-        if N > self.array[step]:  # Если элемент больше среднего в диапазоне
-            self.Left = step
-        if N < self.array[step]:  # Если элемент меньше среднего в диапазоне
-            self.Right = step
-            if self.Left == 0 and self.Right == 1:
-                # self.Right = self.Left
-                self.Right = step = self.Left
-                # step = self.Left
-        if N == self.array[step]:  # Если элемент найден
-            self.result = 1
-            self.Left = self.Right = step  # Нужно ли менять граници при найденном елементе?
-        if self.Left == self.Right and step != N:  # Если элемент не найден
+        if self.array[self.Left] > N or self.array[self.Right] < N:
             self.result = -1
+        mid = self.Left + (self.Right - self.Left) // 2
+        if self.result == 0:
+            if self.array[mid] == N:
+                self.result = 1
+            else:
+                if self.Left == self.Right:
+                    self.result = -1
+                else:
+                    if self.array[mid] > N:
+                        self.Right = mid - 1
+                    elif self.array[mid] < N:
+                        self.Left = mid + 1
 
     def GetResult(self):
         return self.result
-
-
-array = [num for num in range(0, 11)]
-print(array)
-N = -1
-BS = BinarySearch(array)
-
-while BS.result == 0:
-    BS.Step(N)
-# BS.Step(N)
-print('Левый предел: ', BS.Left)
-print('Правый предел: ', BS.Right)
-print('Результат:', BS.result)
-# BS.Step(N)
-# print('Левый предел: ', BS.Left)
-# print('Правый предел: ', BS.Right)
-# print('Результат:', BS.result)
-# BS.Step(N)
-# print('Левый предел: ', BS.Left)
-# print('Правый предел: ', BS.Right)
-# print('Результат:', BS.result)
-# BS.Step(N)
-# print('Левый предел: ', BS.Left)
-# print('Правый предел: ', BS.Right)
-# print('Результат:', BS.result)
-# BS.Step(N)
-# print('Левый предел: ', BS.Left)
-# print('Правый предел: ', BS.Right)
-# print('Результат:', BS.result)
-# BS.Step(N)
-# print('Левый предел: ', BS.Left)
-# print('Правый предел: ', BS.Right)
-# print('Результат:', BS.result)
-# BS.Step(N)
-# print('Левый предел: ', BS.Left)
-# print('Правый предел: ', BS.Right)
-# print('Результат:', BS.result)
-# BS.Step(N)
-# print('Левый предел: ', BS.Left)
-# print('Правый предел: ', BS.Right)
-# print('Результат:', BS.result)
-#
-#
-#
-#
-#
-print(int((0 + 1)//2))
